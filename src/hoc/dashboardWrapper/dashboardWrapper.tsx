224352d2f2d6be53806d31ded7520661af6e2a77
mariapsz <mariapszonka999@gmail.com>
2019-07-04 13:34:35 +0200

    WIP

--- src/hoc/dashboardWrapper/dashboardWrapper.tsx
+++ src/hoc/dashboardWrapper/dashboardWrapper.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Sidebar from "react-sidebar";
-import {Icon, LinkIcon, LinkText} from './dashboardWrapperStyles';
+import {Icon, LinkIcon, LinkText, NavIcon, SideBarWrapper, SideBarChildrenWrapper} from './dashboardWrapperStyles';
+import {deleteCookie, getCookie} from '../../utils/cookies';
+import {COOKIE_NAME_TOKEN, COOKIE_NAME_USER_NAME} from '../../config';
 
 class DashboardWrapper extends React.Component<any, any> {
     constructor(props: any) {
@@ -11,46 +13,59 @@ class DashboardWrapper extends React.Component<any, any> {
         this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
     }
 
+    logoutUser = () => {
+        deleteCookie(COOKIE_NAME_TOKEN);
+        deleteCookie(COOKIE_NAME_USER_NAME);
+        window.location.reload();
+    };
+
+
     onSetSidebarOpen(open: any) {
         this.setState({sidebarOpen: open});
     }
 
+    getSideBarChildren = (): JSX.Element => {
+        let nickname = 'anna';//getCookie(COOKIE_NAME_USER_NAME);
+        return <SideBarChildrenWrapper>
+            <NavIcon onClick={() => this.onSetSidebarOpen(true)}>
+                <Icon className="fa fa-bars"/>
+            </NavIcon>
+            <div>{nickname}</div>
+            <NavIcon>
+                <LinkIcon to='/dashboard'>
+                    <Icon className="fa fa-home"/>
+                </LinkIcon>
+            </NavIcon>
+            <NavIcon>
+                <LinkIcon to='/addItemURLPage'>
+                    <Icon>&#10010;</Icon>
+                </LinkIcon>
+            </NavIcon>
+            <NavIcon>
+                <LinkIcon to='/settings'>
+                    <Icon className="fa fa-gear"/>
+                </LinkIcon>
+            </NavIcon>
+            <NavIcon>
+                <Icon className="fa fa-power-off" onClick={this.logoutUser}/>
+            </NavIcon>
+        </SideBarChildrenWrapper>
+    };
+
     render() {
         return (<div>
-            <Sidebar
-                sidebar={<b>Sidebar content</b>}
-                open={this.state.sidebarOpen}
-                onSetOpen={this.onSetSidebarOpen}
-                styles={{sidebar: {background: "white"}}}
-            >
-                <button onClick={() => this.onSetSidebarOpen(true)}>
-                    Open sidebar
-                </button>
-                <div>
-                    <div>
-                        <div>
-                            <LinkIcon to='/addItemURLPage'>
-                                <div>&#10010;</div>
-                            </LinkIcon>
-                        </div>
-                        <div>
-                            <LinkText to='/login'>Add new item</LinkText>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <LinkIcon to='/register'>
-                                <Icon className="fa fa-home"/>
-                            </LinkIcon>
-                        </div>
-                        <div>
-                            <LinkText to='/dashboard'>Home</LinkText>
-                        </div>
-                    </div>
-                </div>
-            </Sidebar>
+                <SideBarWrapper>
+                <Sidebar
+                    sidebar={<b>Sidebar content</b>}
+                    open={this.state.sidebarOpen}
+                    onSetOpen={this.onSetSidebarOpen}
+                    styles={{sidebar: {background: "white"}}}
+                >
+                    {this.getSideBarChildren()}
+                </Sidebar>
+                </SideBarWrapper>
                 <div>{this.props.children}</div>
-        </div>
+            </div>
         );
     }
 }
--- src/hoc/dashboardWrapper/dashboardWrapperStyles.ts
+++ src/hoc/dashboardWrapper/dashboardWrapperStyles.ts
@@ -5,6 +5,12 @@ export const SideBarWrapper = styled.div`
   width: 10%;
 `;
 
+export const SideBarChildrenWrapper = styled.div`
+  width: 10%;
+  height: 100vh;
+  background-color: #1e7c74;
+`;
+
 export const PageContentWrapper = styled.div`
   float: right;
   width: 90%;
@@ -12,8 +18,8 @@ export const PageContentWrapper = styled.div`
 
 export const LinkIcon = styled(Link)`
   text-decoration: none;
-  width: 100%;
-  height: 100%;
+  //width: 100%;
+  //height: 10vh;
 `;
 
 export const LinkText = styled(Link)`
@@ -21,6 +27,13 @@ export const LinkText = styled(Link)`
 `;
 
 export const Icon = styled.i`
+  //width: inherit;
+  //height: 10vh; 
+`;
+
+export const NavIcon = styled.div`
   width: 100%;
-  height: 100%;
+  height: 10vh;
+  display: inline-block;
+  text-align: center;
 `;
