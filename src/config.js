99ace3558c422cdab9235d5e2e2b5ddd20d0492f
mariapsz <mariapszonka999@gmail.com>
2019-08-09 23:05:33 +0200

    updated backend url

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const COOKIE_NAME_TOKEN = 'priceObserverUserToken';
 const COOKIE_NAME_USER_NAME = 'priceObserverUserName';
-const REGISTER_API_ENDPOINT = 'http://localhost:8000/user/register';
-const LOGIN_API_ENDPOINT = 'http://localhost:8000/user/authenticate';
-const CHANGE_PASSWORD_API_ENDPOINT = 'http://localhost:8000/user/change-password';
-const RESET_PASSWORD_API_ENDPOINT = 'http://localhost:8000/user/reset-password';
-const CHECK_PRODUCT_API_ENDPOINT = 'http://localhost:8000/url/parse-product';
-const ADD_PRODUCT_API_ENDPOINT = 'http://localhost:8000/products/add';
-const EDIT_PRODUCT_API_ENDPOINT = 'http://localhost:8000/products/edit';
-const REMOVE_PRODUCT_API_ENDPOINT = 'http://localhost:8000/products/remove';
-const GET_PRODUCTS_LIST_API_ENDPOINT = 'http://localhost:8000/products/all';
+const REGISTER_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/user/register';
+const LOGIN_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/user/authenticate';
+const CHANGE_PASSWORD_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/user/change-password';
+const RESET_PASSWORD_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/user/reset-password';
+const CHECK_PRODUCT_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/url/parse-product';
+const ADD_PRODUCT_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/products/add';
+const EDIT_PRODUCT_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/products/edit';
+const REMOVE_PRODUCT_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/products/remove';
+const GET_PRODUCTS_LIST_API_ENDPOINT = 'ec2-3-120-141-247.eu-central-1.compute.amazonaws.com:8000/products/all';
 
 export {
     REGISTER_API_ENDPOINT,
