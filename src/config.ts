96e4a9858a312d2cf498c1cb509aa1e8c71146c5
mariapsz <mariapszonka999@gmail.com>
2019-08-20 19:04:00 +0200

    fixed url to remote instead od local

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-//const BASIC_URL = 'http://ec2-52-58-118-196.eu-central-1.compute.amazonaws.com:8000';
-const BASIC_URL = 'http://localhost:8000';
+const BASIC_URL = 'http://ec2-52-58-118-196.eu-central-1.compute.amazonaws.com:8000';
+//const BASIC_URL = 'http://localhost:8000';
 const REGISTER_API_ENDPOINT = `${BASIC_URL}/user/register`;
 const LOGIN_API_ENDPOINT = `${BASIC_URL}/user/authenticate`;
 const CHANGE_PASSWORD_API_ENDPOINT = `${BASIC_URL}/user/change-password`;
