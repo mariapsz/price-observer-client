c90d21139531983b439fdb374dc816fcf53e5e42
mariapsz <mariapszonka999@gmail.com>
2020-06-19 11:40:01 +0200

    changed backend url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const BASIC_URL = 'http://ec2-52-58-118-196.eu-central-1.compute.amazonaws.com:8000';
+const BASIC_URL = 'https://price-observer-backend.herokuapp.com';
 //const BASIC_URL = 'http://localhost:8000';
 const REGISTER_API_ENDPOINT = `${BASIC_URL}/user/register`;
 const LOGIN_API_ENDPOINT = `${BASIC_URL}/user/authenticate`;
